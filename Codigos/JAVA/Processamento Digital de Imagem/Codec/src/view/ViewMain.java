5d55f6b6bb3e467009032e53d06e6dac62ab1146
HorstGab <gabriel.horst.m@gmail.com>
2020-10-24 18:48:51 -0300

    Update ViewMain.java

--- Codigos/JAVA/Processamento Digital de Imagem/Codec/src/view/ViewMain.java
+++ Codigos/JAVA/Processamento Digital de Imagem/Codec/src/view/ViewMain.java
@@ -1,17 +1,9 @@
 package view;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
 import javax.swing.BoxLayout;
+import javax.swing.JFrame;
 
 public class ViewMain extends JFrame {
 
