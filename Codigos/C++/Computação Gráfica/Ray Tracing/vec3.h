e39b6add21bc39fc5a183d2bf4ab4303bcccf296
HorstGab <gabriel.horst.m@gmail.com>
2020-10-29 19:42:43 -0300

    Update vec3.h

--- Codigos/C++/Computação Gráfica/Ray Tracing/vec3.h
+++ Codigos/C++/Computação Gráfica/Ray Tracing/vec3.h
@@ -121,13 +121,14 @@ using color = vec3; //RGB color
 		        return p;
 		    }
 		}
-
+		
 		vec3 random_unit_vector() {
 		    auto a = random_double(0, 2*pi);
 		    auto z = random_double(-1, 1);
 		    auto r = sqrt(1 - z*z);
 		    return vec3(r*cos(a), r*sin(a), z);
 		}
+		
 		vec3 random_in_hemisphere(const vec3& normal) {
 		    vec3 in_unit_sphere = random_in_unit_sphere();
 		    if (dot(in_unit_sphere, normal) > 0.0) // In the same hemisphere as the normal
