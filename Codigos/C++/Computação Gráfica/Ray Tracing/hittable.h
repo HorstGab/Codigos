f6eb063aa36c6ca767cd2e0d8195f1aeb9c0a112
HorstGab <gabriel.horst.m@gmail.com>
2020-10-30 23:15:56 -0300

    Update hittable.h

--- Codigos/C++/Computação Gráfica/Ray Tracing/hittable.h
+++ Codigos/C++/Computação Gráfica/Ray Tracing/hittable.h
@@ -1,17 +1,17 @@
 #ifndef HITTABLE_H
 #define HITTABLE_H
 
-#include "ray.h"
 #include "rtweekend.h"
+#include "ray.h"
 
 class material;
 
 struct hit_record {
     point3 p;
     vec3 normal;
-    shared_ptr<material> mat_ptr;
+    shared_ptr<material> mat_ptr;    
     double t;
-    bool front_face; //rastreamento frontal
+    bool front_face;
 
     inline void set_face_normal(const ray& r, const vec3& outward_normal) {
         front_face = dot(r.direction(), outward_normal) < 0;
