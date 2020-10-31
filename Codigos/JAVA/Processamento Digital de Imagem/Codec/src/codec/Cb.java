d93ca2393f9cc9818fd58346a600395c10936913
HorstGab <gabriel.horst.m@gmail.com>
2020-10-31 00:18:37 -0300

    Create Cb.java

---
+++ Codigos/JAVA/Processamento Digital de Imagem/Codec/src/codec/Cb.java
@@ -0,0 +1,20 @@
+package codec;
+
+public class Cb {
+
+    private int cb;
+
+    public Cb (){
+        this.cb = 0;
+    }
+    public Cb (int cb){
+        this.cb = cb;
+    }
+
+    public int getCb() {
+        return cb;
+    }
+    public void setCb(int cb) {
+        this.cb = cb;
+    }
+}
