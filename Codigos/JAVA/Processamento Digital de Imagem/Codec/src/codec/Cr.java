98a3d05f875167c18f0f7fbafe350c47583d965f
HorstGab <gabriel.horst.m@gmail.com>
2020-10-31 00:18:49 -0300

    Create Cr.java

---
+++ Codigos/JAVA/Processamento Digital de Imagem/Codec/src/codec/Cr.java
@@ -0,0 +1,20 @@
+package codec;
+
+public class Cr {
+
+    private int cr;
+
+    public Cr (){
+        this.cr = 0;
+    }
+    public Cr (int cr){
+        this.cr = cr;
+    }
+
+    public int getCr() {
+        return cr;
+    }
+    public void setCr(int cr) {
+        this.cr = cr;
+    }
+}
