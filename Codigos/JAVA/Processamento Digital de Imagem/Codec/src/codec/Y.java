c73600b9826237664e2af9b8271dfbf7f66f8254
HorstGab <gabriel.horst.m@gmail.com>
2020-10-31 00:18:53 -0300

    Create Y.java

---
+++ Codigos/JAVA/Processamento Digital de Imagem/Codec/src/codec/Y.java
@@ -0,0 +1,19 @@
+package codec;
+
+public class Y {
+    private int y;
+
+    public Y (){
+        this.y = 0;
+    }
+    public Y (int y){
+        this.y = y;
+    }
+
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+}
