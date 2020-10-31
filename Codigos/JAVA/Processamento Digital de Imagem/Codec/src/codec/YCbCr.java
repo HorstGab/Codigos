f662870641843ea03155c6cbcea1779304989f5d
HorstGab <gabriel.horst.m@gmail.com>
2020-10-31 00:18:58 -0300

    Update YCbCr.java

--- Codigos/JAVA/Processamento Digital de Imagem/Codec/src/codec/YCbCr.java
+++ Codigos/JAVA/Processamento Digital de Imagem/Codec/src/codec/YCbCr.java
@@ -7,72 +7,98 @@ import java.io.IOException;
 
 public class YCbCr {
 
-	private int Y;
-	private int Cb;
-	private int Cr;
+	private Y[][] y;
+	private Cb[][] cb;
+	private Cr[][] cr;
+
 	public YCbCr(){
-		this.Y = 0;
-		this.Cb = 0;
-		this.Cr = 0;
+		this.y= null;
+		this.cb = null;
+		this.cr = null;
 	}
-	public YCbCr(int y, int cb, int cr) {
-		this.Y = y;
-		this.Cb = cb;
-		this.Cr = cr;
-	}
-
-	public int getY() {
-		return Y;
+	public YCbCr(Y[][] y, Cb[][] cb, Cr[][] cr){
+		this.y = y;
+		this.cb = cb;
+		this.cr = cr;
 	}
 
-	public void setY(int y) {
-		Y = y;
+	public Y[][] getY() {
+		return y;
+	}
+	public void setY(Y[][] y) {
+		this.y = y;
+	}
+	public Cb[][] getCb() {
+		return cb;
+	}
+	public void setCb(Cb[][] cb) {
+		this.cb = cb;
+	}
+	public Cr[][] getCr() {
+		return cr;
+	}
+	public void setCr(Cr[][] cr) {
+		this.cr = cr;
 	}
 
-	public int getCb() {
-		return Cb;
+	public YCbCr RGBtoYCbCr(BufferedImage img) throws IOException {
+		Y[][] y = new Y[img.getWidth()][img.getHeight()];
+		Cb[][] cb = new Cb[img.getWidth()][img.getHeight()];
+		Cr[][] cr = new Cr[img.getWidth()][img.getHeight()];
+
+		y = rgbtoY(img);
+		cb = rgbtoCb(img);
+		cr = rgbtoCr(img);
+
+		YCbCr ycbcr = new YCbCr(y, cb, cr);
+		return ycbcr;
 	}
 
-	public void setCb(int cb) {
-		Cb = cb;
-	}
+	public Y[][] rgbtoY (BufferedImage img) throws IOException {
+		Y[][] my = new Y[img.getWidth()][img.getHeight()];
+		for (int i = 0; i < img.getWidth(); i++) {
+			for (int j = 0; j < img.getHeight(); j++) {
+				Color pixel = new Color(img.getRGB(i, j));
+				int r = pixel.getRed();
+				int g = pixel.getGreen();
+				int b = pixel.getBlue();
 
-	public int getCr() {
-		return Cr;
-	}
-
-	public void setCr(int cr) {
-		Cr = cr;
-	}
-
-	public YCbCr[][] RGBtoYCbCr(BufferedImage img) throws IOException {
-		YCbCr[][] ycbcrimg = new YCbCr[img.getWidth()][img.getHeight()];
-		
-		for(int i = 0; i < img.getWidth(); i++){
-            for(int j = 0; j < img.getHeight(); j++){ 
-            	
-            	Color pixel = new Color (img.getRGB(i, j));
-            	int r = pixel.getRed();
-            	int g = pixel.getGreen();
-            	int b = pixel.getBlue();
-            	
-            	int y = (int) ((int) (0.299		*r) + (0.587	*g) + (0.114 	*b));
-            	int Cb = (int) ((int)(-0.169	*r) + (-0.331 	*g) + (0.5 		*b));
-            	int Cr = (int) ((int)(0.5		*r) + (-0.419 	*g) + (-0.091	*b));
-            	
-            	ycbcrimg[i][j] = new YCbCr(y,Cb,Cr);
-            }
-		}
-		//printYCbCr(ycbcrimg, img.getWidth(), img.getHeight());
-		return ycbcrimg;
-	}
-
-	public void printYCbCr (YCbCr[][] img, int w, int h){
-		System.out.println("Cb");
-		for(int i = 0; i < w; i++) {
-			for (int j = 1; j < h; j++) {
-				System.out.println(img[i][j].getCb());
+				int y = (int) ((int) (0.299 * r) + (0.587 * g) + (0.114 * b));
+				my[i][j] = new Y(y);
 			}
 		}
+		return my;
+	}
+
+	public Cb[][] rgbtoCb (BufferedImage img) throws IOException {
+		Cb[][] mcb = new Cb[img.getWidth()][img.getHeight()];
+		for (int i = 0; i < img.getWidth(); i++) {
+			for (int j = 0; j < img.getHeight(); j++) {
+				Color pixel = new Color(img.getRGB(i, j));
+				int r = pixel.getRed();
+				int g = pixel.getGreen();
+				int b = pixel.getBlue();
+
+				int cb = (int) ((int)(-0.169	*r) + (-0.331 	*g) + (0.5 		*b));
+				mcb[i][j] = new Cb(cb);
+			}
+		}
+		return mcb;
+	}
+
+	public Cr[][] rgbtoCr (BufferedImage img) throws IOException {
+		Cr[][] mcr = new Cr[img.getWidth()][img.getHeight()];
+		for (int i = 0; i < img.getWidth(); i++) {
+			for (int j = 0; j < img.getHeight(); j++) {
+				Color pixel = new Color(img.getRGB(i, j));
+				int r = pixel.getRed();
+				int g = pixel.getGreen();
+				int b = pixel.getBlue();
+
+				int cr = (int) ((int)(0.5		*r) + (-0.419 	*g) + (-0.091	*b));
+				mcr[i][j] = new Cr(cr);
+			}
+		}
+		return mcr;
 	}
 }
