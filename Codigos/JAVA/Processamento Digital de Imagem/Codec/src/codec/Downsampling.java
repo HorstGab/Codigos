0644e7142a24e9dc0e92dfccadb5ffabb4d08b0a
HorstGab <gabriel.horst.m@gmail.com>
2020-11-01 16:33:37 -0300

    Update Downsampling.java

--- Codigos/JAVA/Processamento Digital de Imagem/Codec/src/codec/Downsampling.java
+++ Codigos/JAVA/Processamento Digital de Imagem/Codec/src/codec/Downsampling.java
@@ -1,10 +1,8 @@
 package codec;
 
-import java.io.IOException;
-
 public class Downsampling {
 
-    public YCbCr downsample (YCbCr img, int w, int h, int n) throws IOException {
+    public YCbCr downsample (YCbCr img, int w, int h, int n) {
 
         Cb[][] cb = new Cb[w/(n/2)][h/(n/2)];
         Cr[][] cr = new Cr[w/(n/2)][h/(n/2)];
@@ -13,23 +11,16 @@ public class Downsampling {
             return null;
         }
 
-        int aux = 1;
-        cb[0][0] = new Cb(img.getCb()[0][0].getCb());
-        cr[0][0] = new Cr(img.getCr()[0][0].getCr());
+        int i, j, x, y;
+        for(i = 0; i < w-1; i += n/2) {
+            for(j = 0; j < h-1; j += n/2) {
+                x = i/(n/2); y = j/(n/2);
 
-        int x = 0;
-        int y = 1;
-        for(int i = 0; i < w; i++){
-            for(int j = 1; j < h; j++){
-                aux++;
-                if(aux == n) {
-                    cb[x][y] = new Cb(img.getCb()[i][j].getCb());
-                    cr[x][y] = new Cr(img.getCr()[i][j].getCr());
-                    y++;
-                }
+                cb[x][y] = new Cb(img.getCb()[i][j].getCb());
+                cr[x][y] = new Cr(img.getCr()[i][j].getCr());
             }
-            x++;
         }
+
         img.setCb(cb);
         img.setCr(cr);
         return img;
