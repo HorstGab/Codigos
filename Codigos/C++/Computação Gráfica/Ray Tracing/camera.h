9efd450c15a3b03bde03f01909cd68fe96e0befc
HorstGab <gabriel.horst.m@gmail.com>
2020-11-03 05:11:59 -0300

    Update camera.h

--- Codigos/C++/Computação Gráfica/Ray Tracing/camera.h
+++ Codigos/C++/Computação Gráfica/Ray Tracing/camera.h
@@ -1,4 +1,4 @@
-#ifndef CAMERA_H
+    #ifndef CAMERA_H
 #define CAMERA_H
 
 #include "rtweekend.h"
@@ -10,25 +10,23 @@ class camera {
             point3 lookat,
             vec3   vup,
             double vfov, // vertical field-of-view in degrees
-            double aspect_ratio,
-            double aperture,
-            double focus_dist
+            double aspect_ratio
         ) {
             auto theta = degrees_to_radians(vfov);
             auto h = tan(theta/2);
             auto viewport_height = 2.0 * h;
             auto viewport_width = aspect_ratio * viewport_height;
             
-            w = unit_vector(lookfrom - lookat);
-            u = unit_vector(cross(vup, w));
-            v = cross(w, u);
+            auto w = unit_vector(lookfrom - lookat);
+            auto u = unit_vector(cross(vup, w));
+            auto v = cross(w, u);
 
             origin = lookfrom;
-            horizontal = focus_dist * viewport_width * u;
-            vertical = focus_dist * viewport_height * v;
-            lower_left_corner = origin - horizontal/2 - vertical/2 - focus_dist*w;
+            horizontal = viewport_width * u;
+            vertical = viewport_height * v;
+            lower_left_corner = origin - horizontal/2 - vertical/2 - w;
 
-            lens_radius = aperture / 2;
+            //lens_radius = aperture / 2;
         }
 
         ray get_ray(double s, double t) const {
