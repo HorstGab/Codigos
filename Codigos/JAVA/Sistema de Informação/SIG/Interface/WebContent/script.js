f7166b6b7ba8c6f3a6ec6325809cb77b6ed13fb7
HorstGab <gabriel.horst.m@gmail.com>
2020-11-12 16:43:01 -0300

    Interface

--- Codigos/JAVA/Sistema de Informação/SIG/Interface/WebContent/script.js
+++ Codigos/JAVA/Sistema de Informação/SIG/Interface/WebContent/script.js
@@ -2,32 +2,54 @@
 class Validator{
     constructor(){
         this.validations = [
-
+            'data-min-length',
         ]
     }
 
     //iniciar a validação dos campos
     validate(form){
         //pegar os inputs
-        let inputs = form.getElementByTagName('input');
+        let inputs = form.getElementsByTagName('input');
+        
 
-        console.log(inputs);
-        //htmlCollection -> Array
-        let inputA rray = [...inputs];
-         
-        console.log(inputArray);
+        //transforma htmlCollection -> Array
+        let inputsArray = [...inputs];
+        
+        //loop nos input e validação
+        inputsArray.forEach(function(input){
+
+            //loop em todas as validações
+            for(let i = 0; this.validations.length > i; i++){
+                //verifica se a validação atual existe no input
+                if(input.getAttribute(this.validations[i]!==null)){
+                    
+                    //limpando a string pra virar o nome de um metodo
+                    let method = this.validations[i].replace('data-','').replace('-','');
+
+                    //valor do input
+                    let value = input.getAttribute(this.validations[i]);
+
+                    //invocar o metodo
+                    this[method](input, value);
+                }
+            }
+        }, this)
+    }
+
+    //verifica o numero minimo de caracteres
+    minlength(input, minValue){
+        
     }
 }
 
-
-
 let form  = document.getElementById("register-form");
 let submit = document.getElementById("btn-submit");
-let Validator = new  Validator();
+let validator = new  Validator();
 
 //evento que dispara as validações
 submit.addEventListener('click', function(e){
 
     e.preventDefault();
-    Validator.validate(form); 
+    validator.validate(form); 
 })
+
