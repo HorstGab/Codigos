b04c7167ad1378d933c04d7fcdb231251558ecd2
HorstGab <34655233+HorstGab@users.noreply.github.com>
2021-02-18 17:37:48 -0300

    Update filtros.py

--- Codigos/Python/PDI/filtros.py
+++ Codigos/Python/PDI/filtros.py
@@ -2,7 +2,7 @@ from cv2 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-path = '/home/gabriel/Documentos/GitHub/Codigos/Codigos/Python/PDI/lotus.png'
+path = './lotus.png'
 img = cv2.imread(path)
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) 
 
@@ -15,5 +15,6 @@ resultado = np.vstack(
     [np.hstack([suave, bin]),
     np.hstack([binI, cv2.bitwise_and(img, img, mask = binI)])]) 
     
-cv2.imshow("Binarização da imagem", resultado)
+#cv2.imshow("Binarização da imagem", resultado)
+cv2.imshow("Binarização da imagem", cv2.resize(resultado, (1280, 720)))
 cv2.waitKey(0)
