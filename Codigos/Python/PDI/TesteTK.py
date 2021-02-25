8e77309eaad791b6c678930883d14345a31631d6
HorstGab <34655233+HorstGab@users.noreply.github.com>
2021-02-25 01:36:16 -0300

    Update TesteTK.py

--- Codigos/Python/PDI/TesteTK.py
+++ Codigos/Python/PDI/TesteTK.py
@@ -21,15 +21,14 @@ def show_img(edged):
 
 def zeroCross():
 	image = imageInit
-
+	image = grayScale()
 	LoG = cv2.Laplacian(image, cv2.CV_16S)
 
-	minLoG = cv2.morphologyEx(LoG, cv2.MORPH_ERODE, np.ones((3,3)))
-	maxLoG = cv2.morphologyEx(LoG, cv2.MORPH_DILATE, np.ones((3,3)))
+	minLoG = cv2.morphologyEx(LoG, cv2.MORPH_ERODE, np.ones((2,2)))
+	maxLoG = cv2.morphologyEx(LoG, cv2.MORPH_DILATE, np.ones((2,2)))
 
 	edged = (np.logical_or(np.logical_and(minLoG < 0,  LoG > 0), np.logical_and(maxLoG > 0, LoG < 0)) * 255).astype('uint8')
 	show_img(ImageTk.PhotoImage(Img.fromarray(edged)))
-
 
 def grayScale():
 	image = imageInit
@@ -51,11 +50,65 @@ def canny():
 def sobel():
 	image = imageInit
 
-	edged = cv2.Sobel(image, cv2.CV_8U, 0, 1, ksize=3)
+	img_sobelx = cv2.Sobel(image,cv2.CV_8U,1,0,ksize=5)
+	img_sobely = cv2.Sobel(image,cv2.CV_8U,0,1,ksize=5)
+	edged = img_sobelx + img_sobely
+
+	#edged = cv2.Sobel(image, cv2.CV_8U, 0, 1, ksize=3)
 	show_img(ImageTk.PhotoImage(Img.fromarray(edged)))
 
- 
+def highpassB():
+	image = imageInit
+	image = grayScale()
 
+	edged = image - cv2.GaussianBlur(image, (0,0),3)+127
+	show_img(ImageTk.PhotoImage(Img.fromarray(edged)))
+	return edged
+
+def highpassAR():
+	image = imageInit
+	image = grayScale()
+
+	reforco = 1.7
+	edged = (image * (reforco - 1)).astype('uint8')  + (image - cv2.GaussianBlur(image, (0,0),3)+127)
+	show_img(ImageTk.PhotoImage(Img.fromarray(edged)))
+
+def lowpassB():
+	image = imageInit
+	image = grayScale()
+
+	edged = cv2.GaussianBlur(image, (0, 0), 3)
+	show_img(ImageTk.PhotoImage(Img.fromarray(edged)))
+
+def lowpassM():
+	image = imageInit
+	image = grayScale()
+
+	edged = cv2.medianBlur(image, 9)
+	show_img(ImageTk.PhotoImage(Img.fromarray(edged)))
+
+def prewitt():
+	image = imageInit
+	image = grayScale()
+
+	edged_gaussian = cv2.GaussianBlur(image,(3,3),0)
+
+	
+	kernelx = np.array([[1,1,1],[0,0,0],[-1,-1,-1]])
+	kernely = np.array([[-1,0,1],[-1,0,1],[-1,0,1]])
+	img_prewittx = cv2.filter2D(edged_gaussian, -1, kernelx)
+	img_prewitty = cv2.filter2D(edged_gaussian, -1, kernely)
+
+	edged = img_prewittx + img_prewitty
+	show_img(ImageTk.PhotoImage(Img.fromarray(edged)))
+
+def log():
+	image = imageInit
+	image = grayScale()
+	edged = cv2.Laplacian(image, cv2.CV_16S)
+
+	show_img(ImageTk.PhotoImage(Img.fromarray(edged)))
+	
 def select_image():
 	# grab a reference to the image panels
 	global panelA, panelB, imageInit
@@ -118,8 +171,26 @@ btn_canny.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
 btn_zeroCross = Button(root, text="Zero Cross",command=zeroCross)
 btn_zeroCross.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
 
+btn_log = Button(root, text="Log",command=log)
+btn_log.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
 btn_sobel = Button(root, text="Sobel",command=sobel)
 btn_sobel.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+btn_prewitt = Button(root, text="Prewitt",command=prewitt)
+btn_prewitt.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+""" btn_lowpassM = Button(root, text="Passa Baixa - Mediana",command=lowpassM)
+btn_lowpassM.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+btn_lowpassB = Button(root, text="Passa Baixa - Média",command=lowpassB)
+btn_lowpassB.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+btn_highpassAR = Button(root, text="Passa Alta - Alto Reforço",command=highpassAR)
+btn_highpassAR.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
+
+btn_highpassB = Button(root, text="Passa Alta - Basico",command=highpassB)
+btn_highpassB.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10") """
 
 btn_grayScale = Button(root, text="Escala de Cinza",command=grayScale)
 btn_grayScale.pack(side="bottom", fill="both", expand="yes", padx="10", pady="10")
