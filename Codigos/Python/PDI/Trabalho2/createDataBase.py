82ea00adb7b6383a6d35c0ab3591210f4ca1eae3
HorstGab <34655233+HorstGab@users.noreply.github.com>
2021-03-31 22:42:26 -0300

    .

--- Codigos/Python/PDI/Trabalho2/createDataBase.py
+++ Codigos/Python/PDI/Trabalho2/createDataBase.py
@@ -1,55 +1,57 @@
 from cv2 import cv2
 import numpy as np
 import sys
+import os
 
 #carrega o arquivo com as informações de classificação
 face_cascate = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-class DataBase:
+# captura os argumentos 
+arg = sys.argv
+os.makedirs(arg[1])
+
     
-    def faceDetection (self, count):
-        #pega o frame 
-        img = self
-        #cconverte para escalas de cinza
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def faceDetection (self, count):
+    #pega o frame 
+    img = self
+    #cconverte para escalas de cinza
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        #realiza a detecção e retorna os valores da localização do rosto
-        faces = face_cascate.detectMultiScale(gray, 1.3, 5)
+    #realiza a detecção e retorna os valores da localização do rosto
+    faces = face_cascate.detectMultiScale(gray, 1.3, 5)
 
-            
-        for(x,y,w,h) in faces:
-            #guarda apenas as informações classificadas como face
-            crop = img[y:y+h, x: x+w]
+        
+    for(x,y,w,h) in faces:
+        #guarda apenas as informações classificadas como face
+        crop = img[y:y+h, x: x+w]
 
+        # try:
             #salva a imagem da região detectada
-            cv2.imwrite("facetoframe%d.jpg" %count, crop)
-            if cv2.waitKey(10) == 27:
-                break
+        cv2.imwrite(arg[1] + "/facetoframe%d.jpg" %count, crop)
+        print("facetoframe%d.jpg salvo!" %count)
+        # except OSError:
+            # print ("Erro ao salvar imagem")
+        
+    
+def extractFrames(self):
+    #abrir o arquivo de video
+    capturaVideo = cv2.VideoCapture(self)
 
+    # lê o primeiro frame e retorna um bool se foi lido corretamente 
+    retval, frame = capturaVideo.read()
 
-    def extractFrames(self):
-        #abrir o arquivo de video
-        capturaVideo = cv2.VideoCapture(self)
-
-        # lê o primeiro frame e retorna um bool se foi lido corretamente 
+    #contador de frames
+    count = 0
+    while retval:
+        #lê os frames restantes a cada interação
         retval, frame = capturaVideo.read()
+        
+        if frame is not None:
+            #a cada frame realiza a detecção da face
+            faceDetection(frame, count)
+            count += 1
+        
 
-        #contador de frames
-        count = 0
-        while retval:
-            #lê os frames restantes a cada interação
-            retval, frame = capturaVideo.read()
-            
-            if frame is not None:
-                #a cada frame realiza a detecção da face
-                DataBase.faceDetection(frame, count)
-                count += 1
-            else :
-                break
-
-            
-
-
-
-DataBase.extractFrames('libras1.mp4')     
-
+if __name__ == '__main__':
+    extractFrames(arg[1] +'.mp4')     
+    print ("Done!")
