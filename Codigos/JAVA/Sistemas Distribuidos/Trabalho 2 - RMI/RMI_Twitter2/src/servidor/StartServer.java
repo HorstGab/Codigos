3d6709aac8da9cd417c64ab7a4f977d384c9b692
HorstGab <34655233+HorstGab@users.noreply.github.com>
2021-07-03 19:56:24 -0300

    Iniciar Servidor Pronto

--- Codigos/JAVA/Sistemas Distribuidos/Trabalho 2 - RMI/RMI_Twitter2/src/servidor/StartServer.java
+++ Codigos/JAVA/Sistemas Distribuidos/Trabalho 2 - RMI/RMI_Twitter2/src/servidor/StartServer.java
@@ -3,7 +3,6 @@ package servidor;
 import java.rmi.Naming;
 
 import common.IServidor;
-import servidor.Servidor;
 
 
 public class StartServer {
@@ -11,9 +10,8 @@ public class StartServer {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		try {
-			java.rmi.registry.LocateRegistry.createRegistry(3031);
 			IServidor b = new Servidor();
-			Naming.rebind("rmi://localhost:3031/twitter", b);
+			Naming.rebind("rmi://localhost/twitter", b);
 			System.out.println("[system] Twitter server is ready.");
 		}catch(Exception e) {
 			System.out.println("Twitter server failed "+e);
